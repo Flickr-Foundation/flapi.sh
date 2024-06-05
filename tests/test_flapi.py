c9008177a131908c1a6fd8fea319014ca44b9769
Alex Chan <alex@alexwlchan.net>
2024-06-05 09:01:47 +0100

    Add a test for the happy path

--- tests/test_flapi.py
+++ tests/test_flapi.py
@@ -10,3 +10,12 @@ def test_no_method_is_error():
     assert proc.returncode == 1
     assert stdout == b""
     assert stderr.startswith(b"Usage: ")
+
+
+def test_can_call_an_api():
+    proc = subprocess.Popen(["./flapi", "flickr.profile.getProfile", "user_id=197130754@N07"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = proc.communicate()
+
+    assert proc.returncode == 0
+    assert b'Flickr Foundation' in stdout
+    assert stderr == b""
