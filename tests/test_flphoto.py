96a97af1e1585b9ead353b75e8919d58e6d22acd
Alex Chan <alex@alexwlchan.net>
2024-08-30 15:10:40 +0100

    Add some tests for `flphoto`

---
+++ tests/test_flphoto.py
@@ -0,0 +1,41 @@
+"""
+Tests for the ``flphoto`` script.
+"""
+
+from utils import get_failure_stderr, get_success_stdout
+
+
+def test_no_method_is_error(flickr_api_key: None) -> None:
+    """
+    Calling ``flphoto`` without a photo ID is an error.
+    """
+    stderr = get_failure_stderr(cmd=["flphoto"])
+    assert stderr.startswith(b"Usage: ")
+
+
+def test_too_many_arguments_is_error(flickr_api_key: None) -> None:
+    """
+    Calling ``flphoto`` with more than one argument is an error.
+    """
+    stderr = get_failure_stderr(cmd=["flphoto"])
+    assert stderr.startswith(b"Usage: ")
+
+
+def test_can_look_up_a_photo_id(flickr_api_key: None) -> None:
+    """
+    Call ``flphoto`` with a photo ID.
+    """
+    stdout = get_success_stdout(cmd=["flphoto", "53630778857"])
+
+    assert b"https://www.flickr.com/photos/flickrfoundation/53630778857/" in stdout
+
+
+def test_can_look_up_a_photo_url(flickr_api_key: None) -> None:
+    """
+    Call ``flphoto`` with a photo URL.
+    """
+    stdout = get_success_stdout(
+        cmd=["flphoto", "https://www.flickr.com/photos/flickrfoundation/53630778857/"]
+    )
+
+    assert b"https://www.flickr.com/photos/flickrfoundation/53630778857/" in stdout
